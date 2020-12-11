93115069d37b98bbaeb0358547169b3cebc19a88
aram <leandrojavr@gmail.com>
2020-12-11 10:22:01 -0300

    Day 5 part 1 done

--- day 5/p1/src/main.rs
+++ day 5/p1/src/main.rs
@@ -5,34 +5,69 @@ mod tests {
     use super::*;
 
     #[test]
-    fn binary_search() {
-        assert_eq!(find_row("FBFBBFF"), 44);
-        assert_eq!(find_row("BFFFBBF"), 70);
-        assert_eq!(find_row("FFFBBBF"), 14);
-        assert_eq!(find_row("BBFFBBF"), 102);
-        assert_eq!(find_row("FFFFFFF"), 0);
-        assert_eq!(find_row("BBBBBBB"), 127);
+    fn test_find_row() {
+        assert_eq!(partition_space("FBFBBFF", 'B', 'F', 127), 44);
+        assert_eq!(partition_space("BFFFBBF", 'B', 'F', 127), 70);
+        assert_eq!(partition_space("FFFBBBF", 'B', 'F', 127), 14);
+        assert_eq!(partition_space("BBFFBBF", 'B', 'F', 127), 102);
+        assert_eq!(partition_space("FFFFFFF", 'B', 'F', 127), 0);
+        assert_eq!(partition_space("BBBBBBB", 'B', 'F', 127), 127);
+    }
+
+    #[test]
+    fn test_find_column() {
+        assert_eq!(partition_space("RLR", 'R', 'L', 7), 5);
+        assert_eq!(partition_space("RRR", 'R', 'L', 7), 7);
+        assert_eq!(partition_space("RLL", 'R', 'L', 7), 4);
+        assert_eq!(partition_space("LLL", 'R', 'L', 7), 0);
+    }
+
+    #[test]
+    fn test_find_id() {
+        let row = partition_space("BFFFBBF", 'B', 'F', 127);
+        let column = partition_space("RRR", 'R', 'L', 7);
+        assert_eq!(find_id(row, column), 567);
+
+        let row = partition_space("FFFBBBF", 'B', 'F', 127);
+        let column = partition_space("RRR", 'R', 'L', 7);
+        assert_eq!(find_id(row, column), 119);
+
+        let row = partition_space("BBFFBBF", 'B', 'F', 127);
+        let column = partition_space("RLL", 'R', 'L', 7);
+        assert_eq!(find_id(row, column), 820);
     }
 }
 
-fn find_row(bs : &str) -> u32 {
+fn find_id(row : u32, column : u32) -> u32 {
+    return (row * 8) + column;
+}
+
+fn partition_space(bs : &str, upper_comparator : char,
+                   lower_comparator: char, initial : u32) -> u32 {
     let mut lower : u32 = 0;
-    let mut upper : u32 = 127;
+    let mut upper : u32 = initial;
     for i in bs.chars() {
-        if i == 'F' {
+        if i == lower_comparator {
                 upper = upper - ((upper - lower) as f64 / 2.0).round() as u32;
-        } else if i == 'B' {
+        } else if i == upper_comparator {
                 lower = lower + ((upper - lower) as f64 / 2.0).round() as u32;
         }
     }
 
-    println!("{} - {}", upper, lower);
     return upper;
+}
+
+fn map_data_to_ids(data : &str) -> u32 {
+    let row = partition_space(&data[0..7], 'B', 'F', 127);
+    let column = partition_space(&data[7..data.len()], 'R', 'L', 7);
+    find_id(row, column)
 }
 
 fn main() {
     let txt = fs::read_to_string("../input.txt").expect("Error opening file");
 
-    let mut positions : Vec<&str> = txt.split_terminator("\n").collect::<Vec<&str>>();
+    let max_id : u32 = txt.split_terminator("\n")
+                          .map(map_data_to_ids).max().unwrap();
 
+    println!("The highest id is {}", max_id);
 }
