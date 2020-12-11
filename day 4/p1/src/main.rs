27483018d55a5be1e9d73babf6da1a62dc3103a1
aram <leandrojavr@gmail.com>
2020-12-11 00:04:55 -0300

    Part 4 unfinished..

--- day 4/p1/src/main.rs
+++ day 4/p1/src/main.rs
@@ -1,43 +1,84 @@
 use std::fs;
 use std::collections::BTreeMap;
 
-fn check_valid_passport(passport : Vec<&str>) -> bool {
-    let mut pointers : BTreeMap<&str, Box<dyn Fn (&str) -> bool>> = BTreeMap::new();
-    pointers.insert("byr", Box::new(| x : &str | x.len() > 5));
-    pointers.insert("iyr", Box::new(| x : &str | x.len() > 5));
-    pointers.insert("eyr", Box::new(| x : &str | x.len() > 5));
-    pointers.insert("hgt", Box::new(| x : &str | x.len() > 5));
-    pointers.insert("hcl", Box::new(| x : &str | x.len() > 5));
-    pointers.insert("ecl", Box::new(| x : &str | x.len() > 5));
-    pointers.insert("pid", Box::new(| x : &str | x.len() > 5));
-    pointers.insert("cid", Box::new(| x : &str | x.len() > 5));
+fn check_valid_passport(passport : &Vec<&str>) -> bool {
+    let mut pointers : BTreeMap<&str, fn (&str) -> usize> = BTreeMap::new();
+    pointers.insert("byr", | x : &str | 2);
+    pointers.insert("iyr", | x : &str | 3 );
+    pointers.insert("eyr", | x : &str | 5);
+    pointers.insert("hgt", | x : &str | 7);
+    pointers.insert("hcl", | x : &str | 11);
+    pointers.insert("ecl", | x : &str | 13);
+    pointers.insert("pid", | x : &str | 17);
+    pointers.insert("cid", | x : &str | 19);
 
+    if (passport.len() <= 6) {
+        return false;
+    }
+
+    let mut result = 1;
     for e in passport {
         match pointers.get(&e[0..3]) {
-            None => if e[5..e.len()] != *"cid" { return false; }
-            Some(v) => if !v(&e[5..e.len()]) { return false }
+            None => { }
+            Some(v) => {
+                         result = result * v(&e[0..3]);
+                       }//if !v(&e[5..e.len()]) { return false }
         };
     }
-    return true;
+
+    if result == 9699690 || result == 510510 {
+        return true;
+    }
+    return false;
+}
+
+fn compact(input : &str) -> Vec<&str> {
+    let mut result : Vec<&str> = Vec::new();
+    let input = input.split("\n").collect::<Vec<&str>>();
+    for i in input {
+        let mut c = i.split(" ").collect::<Vec<&str>>();
+        result.append(&mut c);
+    }
+
+    return result;
 }
 
 fn main() {
     let txt = fs::read_to_string("../input.txt").expect("Error opening file");
 
-    let mut passports : Vec<Vec<&str>> = txt.split("\n\n")
-                                      .map(|w| w.split(" ").collect::<Vec<_>>())
-                                      .collect::<Vec<_>>();
+    let mut passports : Vec<&str> = txt.split("\n\n").collect::<Vec<&str>>();
+    println!("Total passports {}", passports.len());
     passports.pop();
 
+    let passports : Vec<Vec<&str>> = passports.iter().map(|w| compact(w)).collect::<Vec<Vec<&str>>>();
+
+
+    println!("Passports after fmt {}", passports.len());
+
+    for i in &passports {
+        for k in i {
+            println!("{}", k);
+        }
+        println!("\n\n");
+    }
     // we get a vector of passports (Vec<&str>)
+
 
     let mut valid_passports : usize = 0;
     for s in passports {
-        if check_valid_passport(s) {
-            valid_passports = valid_passports + 1;
+        if s.len() > 0 {
+            if check_valid_passport(&s) {
+                valid_passports = valid_passports + 1;
+            } else {
+                println!("{}", s.len());
+                for e in s {
+                    print!("{} ", e);
+                }
+                println!("");
+            }
         }
     }
 
-
     println!("Number of valid passports: {}", valid_passports);
+
 }
